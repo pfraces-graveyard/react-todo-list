c33f18ad603634e7da2bc5e863797c8460cdd717
Pau Fracés <pfraces@gmail.com>
2020-02-06 22:18:47 +0100

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [state, setState] = useState({
-    newItem: 'what to do?',
-    filter: 'All',
+    newItem: "what to do?",
+    filter: "All",
     items: [
-      { text: 'foo', done: false, id: 0 },
-      { text: 'bar', done: true, id: 1 },
-      { text: 'qux', done: false, id: 2 },
+      { text: "foo", done: false, id: 0 },
+      { text: "bar", done: true, id: 1 },
+      { text: "qux", done: false, id: 2 }
     ]
   });
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setState({ ...state, newItem: e.target.value });
   };
 
   const handleClick = () => {
     setState({
-      newItem: '',
-      items: [...state.items, { text: state.newItem, done: false, id: state.items.length }]
+      newItem: "",
+      items: [
+        ...state.items,
+        { text: state.newItem, done: false, id: state.items.length }
+      ]
     });
   };
 
-  const toggleStatus = (id) => {
+  const toggleStatus = id => {
     setState({
       ...state,
-      items: state.items.map(item => item.id === id ? { ...item, done: !item.done } : item)
+      items: state.items.map(item =>
+        item.id === id ? { ...item, done: !item.done } : item
+      )
     });
   };
 
-  const handleFilterChange = (filter) => {
+  const handleFilterChange = filter => {
     setState({
       ...state,
       filter: filter
@@ -39,32 +44,67 @@ function App() {
 
   return (
     <div className="App">
-      <input onChange={handleChange} value={state.newItem} />
-      <button onClick={handleClick}>+</button>
-      <div className="filter">
-        <label>
-          <input type="radio" name="filter" onChange={ () => handleFilterChange('All') } checked={state.filter === 'All'} />
-          All
-        </label>
-        <label>
-          <input type="radio" name="filter" onChange={ () => handleFilterChange('Pending') } checked={state.filter === 'Pending'} />
-          Pending
-        </label>
-        <label>
-          <input type="radio" name="filter" onChange={ () => handleFilterChange('Done') } checked={state.filter === 'Done'} />
-          Done
-        </label>
-      </div>
-      <div className="list">
-        {state.items
-          .filter(item => {
-            if (state.filter === 'Pending') { return !item.done; }
-            if (state.filter === 'Done') { return item.done; }
-            return true;
-          })
-          .map(item => (
-            <div onClick={() => toggleStatus(item.id)} className={ item.done ? 'item done' : 'item' } key={item.id}>{item.text}</div>
-          ))}
+      <div className="container">
+        <div className="new-item">
+          <input
+            className="add-input"
+            onChange={handleChange}
+            value={state.newItem}
+          />
+          <button className="add" onClick={handleClick}>
+            +
+          </button>
+        </div>
+        <div className="filter">
+          <label>
+            <input
+              type="radio"
+              name="filter"
+              onChange={() => handleFilterChange("All")}
+              checked={state.filter === "All"}
+            />
+            All
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="filter"
+              onChange={() => handleFilterChange("Pending")}
+              checked={state.filter === "Pending"}
+            />
+            Pending
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="filter"
+              onChange={() => handleFilterChange("Done")}
+              checked={state.filter === "Done"}
+            />
+            Done
+          </label>
+        </div>
+        <div className="list">
+          {state.items
+            .filter(item => {
+              if (state.filter === "Pending") {
+                return !item.done;
+              }
+              if (state.filter === "Done") {
+                return item.done;
+              }
+              return true;
+            })
+            .map(item => (
+              <div
+                onClick={() => toggleStatus(item.id)}
+                className={item.done ? "item done" : "item"}
+                key={item.id}
+              >
+                {item.text}
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
