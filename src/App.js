ede7b9f663615663af1968d01319ed7c315b5fb4
Pau Fracés <pfraces@gmail.com>
2020-02-06 20:59:52 +0100

    added feature to add items

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({
+    newItem: 'what to do?',
+    items: [
+      { text: 'foo', done: false },
+      { text: 'bar', done: false },
+      { text: 'qux', done: false },
+    ]
+  });
+
+  const handleChange = (e) => {
+    setState({ ...state, newItem: e.target.value });
+  };
+
+  const handleClick = () => {
+    setState({
+      newItem: '',
+      items: [...state.items, { text: state.newItem, done: false }]
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={handleChange} value={state.newItem} />
+      <button onClick={handleClick}>+</button>
+      <div className="list">
+        {state.items.map(item => (
+          <div className="item">{item.text}</div>
+        ))}
+      </div>
     </div>
   );
 }
